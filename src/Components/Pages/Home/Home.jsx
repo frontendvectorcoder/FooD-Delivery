bff5acefef85d088232192b73cf1779a9149cf08
HaiderAliGill <haidergill09876@gmail.com>
2024-08-22 00:29:52 +0500

    new

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import SectionOne from './SectionOne'
 import SectionTwo from './SectionTwo'
 import SectionThree from './SectionThree'
 import SectionFour from './SectionFour'
+import SectionFive from './SectionFive'
 
 function Home() {
   return (
@@ -24,6 +25,9 @@ function Home() {
 
         <SectionFour />
 
+        {/* SECTION FOUR  */}
+
+        <SectionFive />
 
       </Layout>
     </>
--- src/Components/Pages/Home/SectionFive.jsx
+++ src/Components/Pages/Home/SectionFive.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import storIOS from "../../../assets/shop/appstore.png";
+import storeis from "../../../assets/shop/googleplay.png";
+import eshop from "../../../assets/shop/e-shop.png";
 
 function SectionFive() {
   return (
-    <div>SectionFive</div>
-  )
+    <>
+      <section className="shop_section">
+        <Container>
+          <Row className=" align-content-center">
+            <Col lg={6} className=" text-center text-lg-start mb-5 mb-lg-0">
+              <h4>Lorem ipsum dolor sit amet.</h4>
+              <h2>save upto 50%</h2>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+                blanditiis architecto inventore mollitia doloribus tempore
+                repellat quasi deserunt laboriosam cumque.
+              </p>
+              <Link to="/">
+                <img src={storIOS} alt="image" className=" img-fluid me-3" />
+              </Link>
+              <Link to="/">
+                <img src={storeis} alt="image" className=" img-fluid me-3" />
+              </Link>
+            </Col>
+            <Col lg={6}>
+              <img src={eshop} className=" img-fluid" alt="shop name" />
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
 
-export default SectionFive
+export default SectionFive;
