0a0fc49df60b4df9d376852f1a66f45e5def154f
Haider Ali <frontend.vectorcoder@gmail.com>
2024-08-22 12:01:30 +0500

    section five

--- src/Components/Pages/Home/SectionFive.jsx
+++ src/Components/Pages/Home/SectionFive.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import storIOS from "../../../assets/shop/appstore.png";
 import storeis from "../../../assets/shop/googleplay.png";
 import eshop from "../../../assets/shop/e-shop.png";
+import brand1 from "../../../assets/brands/brand-11.png"
+import brand2 from "../../../assets/brands/brand-12.png"
+import brand3 from "../../../assets/brands/brand-13.png"
+import brand4 from "../../../assets/brands/brand-14.png"
+import brand5 from "../../../assets/brands/brand-15.png"
+import brand6 from "../../../assets/brands/brand-16.png"
+import brand7 from "../../../assets/brands/brand-17.png"
 
 function SectionFive() {
   return (
@@ -20,15 +27,65 @@ function SectionFive() {
                 repellat quasi deserunt laboriosam cumque.
               </p>
               <Link to="/">
-                <img src={storIOS} alt="image" className=" img-fluid me-3" />
+                <img src={storIOS} alt="image" className=" img-fluid me-3 store" />
               </Link>
               <Link to="/">
-                <img src={storeis} alt="image" className=" img-fluid me-3" />
+                <img src={storeis} alt="image" className=" img-fluid me-3 store" />
               </Link>
             </Col>
             <Col lg={6}>
               <img src={eshop} className=" img-fluid" alt="shop name" />
             </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="brand_section">
+        <Container>
+          <Row>
+            <Carousel>
+              <Carousel.Item>
+                <Carousel.Caption>
+                  <div className=" d-flex align-items-center justify-content-between">
+                    <div className="brand_img">
+                      <img src={brand1} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand2} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand3} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand4} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand5} className=" img-fluid" alt="Beands" />
+                    </div>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <Carousel.Caption>
+                  <div className=" d-flex align-items-center justify-content-between">
+                    <div className="brand_img">
+                      <img src={brand3} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand4} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand5} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand6} className=" img-fluid" alt="Beands" />
+                    </div>
+                    <div className="brand_img">
+                      <img src={brand7} className=" img-fluid" alt="Beands" />
+                    </div>
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
           </Row>
         </Container>
       </section>
