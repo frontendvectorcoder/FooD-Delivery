d0bd22a12eb17e02c73b59db6f97d6478e2584f2
Haider Ali <frontend.vectorcoder@gmail.com>
2024-09-02 12:17:36 +0500

    citys

--- src/Components/Pages/Shop/ShopDown.jsx
+++ src/Components/Pages/Shop/ShopDown.jsx
@@ -3,91 +3,94 @@ import { Container, Row, Col } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 
 const countries = [
-  {
-    name: "Select",
-    value: "disabled",
-    disabled: "disabled",
-    selected: "true",
-  },
-  {
-    name: "USA",
-    value: "usa",
-    cities: ["new york", "york"],
-  },
-  {
-    name: "pakistan",
-    value: "pk",
-    cities: ["Faisalabad", "Lahore"],
-  },
-  {
-    name: "Bangladesh",
-    value: "bn",
-    cities: ["Dhaka", "Chittagong"],
-  },
+    {
+        name: "Select",
+        value: "disabled",
+        disabled: "disabled",
+    },
+    {
+        name: "USA",
+        value: "usa",
+        cities: ["New york", "Los Angeles"],
+    },
+    {
+        name: "pakistan",
+        value: "pk",
+        cities: ["Faisalabad", "Lahore"],
+    },
+    {
+        name: "Bangladesh",
+        value: "bn",
+        cities: ["Dhaka", "Chittagong"],
+    },
 ];
 
 function ShopDown() {
-  const [disable, setdisable] = useState(true);
-  const [second, setsecond] = useState([]);
+    const [disable, setdisable] = useState(true);
+    const [second, setsecond] = useState();
 
-  let Selectedcity = (choice) => {
-    console.log(choice)
+    let Selectedcity = (choice) => {
+        console.log(choice)
 
-   let ismatch = countries.find(item => item.value === choice)
+        let ismatch = countries.find(item => item.value === choice)
 
-    if (ismatch && ismatch.cities) {
-        console.log("ssssssssssssssssssssssssss")
-        setsecond(ismatch.cities)
-        setdisable(false)
-        console.log(second.value , "gggggggggg")
-    } else {
-        setsecond([]);
-    }
-
+        if (ismatch && ismatch.cities) {
+            setsecond(ismatch.cities)
+            setdisable(false)
+        } else {
+            setsecond([]);
+            setdisable(true)
+        }
 
 
-    // if (choice) {
-    //   let newvalue = countries.map((item) => item);
-    //   console.log(newvalue);
-    //     for (let index = 0; index < newvalue.length; index++) {
-    //       let secondselect = newvalue[index];
-    //       console.log(secondselect)
-    //       if (choice == secondselect[index].value) {
-    //         console.log("dddddddddddddddddddddddddddddddddddd")
-    //       }
-    //     }
-    // }
-  };
 
-  return (
-    <section className=" pb-5" style={{ backgroundColor: "#111b22" }}>
-      <Container>
-        <Row className=" justify-content-center align-items-center d-flex pt-5 pb-5 text-white">
-          <Col lg={{ span: 4 }} className="pt-5">
-            <Form.Select
-              onChange={(choice) => Selectedcity(choice.target.value)}
-            >
-              {countries.map((country, index) => (
-                <option
-                  value={country.value}
-                  key={index}
-                  disabled={country.disabled}
-                  selected={country.selected}
-                >
-                  {country.name}
-                </option>
-              ))}
-            </Form.Select>
-          </Col>
-          <Col lg={{ span: 4 }} className="pt-5">
-            <Form.Select disabled={disable} aria-label="Default select example">
-              {disable == "true" ? "not" : <option>{second}</option>}
-            </Form.Select>
-          </Col>
-        </Row>
-      </Container>
-    </section>
-  );
+        // if (choice) {
+        //   let newvalue = countries.map((item) => item);
+        //   console.log(newvalue);
+        //     for (let index = 0; index < newvalue.length; index++) {
+        //       let secondselect = newvalue[index];
+        //       console.log(secondselect)
+        //       if (choice == secondselect[index].value) {
+        //         console.log("dddddddddddddddddddddddddddddddddddd")
+        //       }
+        //     }
+        // }
+    };
+
+    return (
+        <section className=" pb-5" style={{ backgroundColor: "#111b22" }}>
+            <Container>
+                <Row className=" justify-content-center align-items-center d-flex pt-5 pb-5 text-white">
+                    <Col lg={{ span: 4 }} className="pt-5">
+                        <Form.Select
+                            onChange={(choice) => Selectedcity(choice.target.value)}
+                        >
+                            {countries.map((country, index) => (
+                                <option
+                                    value={country.value}
+                                    key={index}
+                                    disabled={country.disabled}
+                                >
+                                    {country.name}
+                                </option>
+                            ))}
+                        </Form.Select>
+                    </Col>
+                    <Col lg={{ span: 4 }} className="pt-5">
+                        <Form.Select disabled={disable} aria-label="Default select example">
+                            {disable ? (
+                                <option disabled>not</option>
+                            ) : (
+                                second.map((city , index) =>(
+                                    <option key={index} value={city}>{city}</option>
+                                ))
+                            )}
+                        </Form.Select>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
 }
 
 export default ShopDown;
