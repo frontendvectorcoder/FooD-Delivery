e7f9bce1de8bf24592449e3a8604e6d1ab1303c6
Haider Ali <frontend.vectorcoder@gmail.com>
2024-09-06 18:53:59 +0500

    new

--- src/Components/Pages/Dark/ContentApi.jsx
+++ src/Components/Pages/Dark/ContentApi.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
-export const ContentApi = () => {
-  return (
-    <div>ContentApi</div>
-  )
+const themesapply = createContext()
+
+export const ContentApi = ({ children }) => {
+
+    let [checklocal, setchecklocal] = useState('light')
+
+
+    useEffect(() => {
+
+        const theme = localStorage.getItem('theme')
+
+        if (theme) {
+            setchecklocal(theme)
+        }
+
+
+    }, [])
+
+    let themetoggle = () => {
+        let newtheme = checklocal === 'light' ? 'dark' : 'light';
+        localStorage.setItem(newtheme)
+    }
+
+
+
+
+    return (
+        <themesapply.Provider>
+            {children}
+        </themesapply.Provider>
+    )
 }
