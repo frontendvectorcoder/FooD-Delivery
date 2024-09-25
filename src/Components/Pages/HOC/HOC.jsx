0999947d0fb02f6af921261475f4295376e311a8
Haider Ali <frontend.vectorcoder@gmail.com>
2024-09-25 18:33:45 +0500

    new

--- src/Components/Pages/HOC/HOC.jsx
+++ src/Components/Pages/HOC/HOC.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../Layout/Layout";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const HOC = (Component) => {
   return (
@@ -9,7 +9,10 @@ const HOC = (Component) => {
         <Container>
           <Row className=" justify-content-center align-items-center d-flex pt-5 pb-5 text-white">
             <Col lg={{ span: 12 }} className="pt-5">
-
+              {/* <Count /> */}
+              <CountD com={Count} />
+              <CountYELLOW com={Count} />
+              <CountRED com={Count} />
             </Col>
           </Row>
         </Container>
@@ -17,5 +20,41 @@ const HOC = (Component) => {
     </Layout>
   );
 };
+
+function Count() {
+  let [count, setcount] = useState(0);
+
+  return (
+    <Button
+      onClick={() => {
+        setcount(count + 5);
+      }}
+    >
+      {count}
+    </Button>
+  );
+}
+
+function CountD(props) {
+  return (
+    <h2 style={{ backgroundColor: "red" }}>
+      <props.com />
+    </h2>
+  );
+}
+function CountRED(props) {
+  return (
+    <h2 style={{ backgroundColor: "green" }}>
+      <props.com />
+    </h2>
+  );
+}
+function CountYELLOW(props) {
+  return (
+    <h2 style={{ backgroundColor: "yellow" }}>
+      <props.com />
+    </h2>
+  );
+}
 
 export default HOC;
